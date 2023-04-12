5003273072de1b79f7859e5bf14118242f429da7
Nathaniel Therrien <mo38175@umbc.edu>
2023-04-12 16:44:22 -0400

    It's coming together :)

--- beta.py
+++ beta.py
@@ -7,6 +7,7 @@ import chess.svg
 import cairosvg
 from PIL import Image,ImageTk
 
+import threading
 
 # Constants
 DATAFILE = 'games.csv'
@@ -78,6 +79,8 @@ def get_top_five():
 
 # State pruning
 def prune(move):
+    global current_states
+
     new_states = []
     for i in range(len(current_states)):
         if current_states[i][1][0] == move:
@@ -86,24 +89,77 @@ def prune(move):
                 new_states.append((current_states[i][0], trimmed_state))
     current_states = new_states
 
-if __name__ == '__main__':
-    # Setup stuff
-    load_data()
-    while ((player != 1) and (player != 2)):
-        print('What piece are you?\n1. White\n2. Black')
-        player = int(input())
-    board = chess.Board()
+# Convert to the notation that helps make an arrow
+def san_to_lan(move, board):
+    piece_map = {'P' : 1, 'N': 2, 'B' : 3, 'R' : 4, 'Q' : 5, 'K' : 6}
+
+    piece = 'P'
+
+    # Normal move
+    if (move[0].isupper()) and (move[0] != 'O'):
+        piece = move[0]
+
+    # Castle move
+    elif move[0] == 'O':
+        if move == 'O-O':
+            if board.turn == chess.WHITE:
+                return 'e1g1'
+            else:
+                return 'e8g8'
+        else:
+            if board.turn == chess.WHITE:
+                return 'e1c1'
+            else:
+                return 'e8c8'
+
+    destination_square = move[-2:]
+
+    possible_moves = []
+    for move in board.generate_legal_moves():
+        if move.to_square == chess.parse_square(destination_square):
+            if (board.piece_type_at(move.from_square) == piece_map[piece]):
+                #print(move, board.piece_type_at(move.from_square))
+                possible_moves.append(move)
+    #print('LAN format possible moves', possible_moves)
+    return possible_moves[0]
+
+def loop():
+    global turn_counter
+
+    # Window element that holds the svg image
+    chess_window = tk.Label(window)
 
     # Engine loop
     while True:
+
         # Who's turn is it?
         if turn_counter % 2 == 0:
             print('==WHITE\'S TURN==')
         else:
             print('==BLACK\'S TURN==')
 
-        # Render the board with the top five arrows
-        get_top_five()
+        # Get the top 5 moves in LAN format
+        five_moves = get_top_five()
+        print('Top 5 Moves', five_moves)
+        lan_five_moves = []
+        for i in range(len(five_moves)):
+            lan_five_moves.append(san_to_lan(five_moves[i][0], board))
+
+        # Make the render
+        arrows = []
+        for move in lan_five_moves:
+            arrows.append(chess.svg.Arrow(move.from_square, move.to_square))
+        render = chess.svg.board(board, arrows=arrows)
+
+        # Display the new image
+        with open('temp.svg', 'w') as f:
+            f.write(render)
+        f.close()
+        image_data = cairosvg.svg2png(url="temp.svg")
+        image = Image.open(io.BytesIO(image_data))
+        tk_image = ImageTk.PhotoImage(image)
+        chess_window.configure(image=tk_image)
+        chess_window.pack()
 
         # Await a legal move
         move = get_move(board)
@@ -114,4 +170,12 @@ if __name__ == '__main__':
         # Increment turn
         turn_counter += 1
 
-        # Repeat
+if __name__ == '__main__':
+    window = tk.Tk()
+    load_data()
+    while ((player != 1) and (player != 2)):
+        print('What piece are you?\n1. White\n2. Black')
+        player = int(input())
+    board = chess.Board()
+    loop()
+    window.mainloop()
